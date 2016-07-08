f11a6d3d3f278d426b962a3acfb60d0838ed5184
jonathan-fulton <jonathan@videoblocks.com>
2016-07-08 15:25:09 -0400

    Adding back error on invalid key but providing optional flag on _addProperties so consuming VOs can choose to throw errors or not

--- index.js
+++ index.js
@@ -12,10 +12,35 @@ class VO {
         this._immutableMap = Immutable.Map();
     }
 
-    _addProperties(object) {
+    /**
+     * @param {Object} object
+     * @param {Boolean} [dontThrowErrorOnInvalidKey=false] - use this flag if you use VOs to represent db tables; allows de-coupling database migrations and code changes, a necessity in production
+     * @private
+     */
+    _addProperties(object, dontThrowErrorOnInvalidKey) {
+        dontThrowErrorOnInvalidKey = dontThrowErrorOnInvalidKey || false;
+
         let map = this._immutableMap.toJS();
         Object.assign(map, object);
         this._immutableMap = Immutable.Map(map);
+
+        if (!dontThrowErrorOnInvalidKey) {
+
+            /**
+             * Note, this is a bit of a hack.  Here's the logic:
+             * 1) Set all the values passed to us
+             * 2) Check that we can retrieve all the values using the expected getters (they should match the object keys)
+             * 3) Throw an error if we get an undefined value back b/c that implies the getter doesn't exist
+             *
+             * I would like to simply validate the object keys against list of getters but
+             * there's no node.js function that gets those right now, at least that I could find.
+             */
+            Object.keys(object).forEach(key => {
+                if (typeof(this[key]) === 'undefined' && typeof(object[key]) !== 'undefined') {
+                    throw new Error(`Invalid object key (${key}) passed to VO _addProperties.  Remove the key or define an associated method.`);
+                }
+            });
+        }
     };
 
     // @TODO: deprecate this method
